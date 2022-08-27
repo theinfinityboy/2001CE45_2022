1e756de780fa32f3d0f7c4198ecdd4035eb17690
Raja Babu <rajababuk984@gmail.com>
2022-08-27 23:16:08 +0530

    tut01 code

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,13 @@
-print("hello world")
+def factorial( x):
+    i =1
+    fact = 1
+    if x==0:
+        print(fact)
+    else:
+        while i<=x:
+            fact=fact*i
+            i=i+1  
+        print(fact)
+    
+x=int(input("Enter the number whose factorial is to be found\n"))
+factorial(x)
