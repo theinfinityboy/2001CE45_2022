0eddd2c333bba227ad4f2846ffe8b2df654fe4de
Raja Babu <rajababuk984@gmail.com>
2022-09-15 00:46:26 +0530

    files downloaded and committed
    files pulled from course github public repo then pushed to my private one

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,1 +1,13 @@
-print("we are in the tut 2")
+def octant_transition_count(mod=5000):
+###Code
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octant_transition_count(mod)
