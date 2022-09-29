0017dca84b1393aaa1b5922feb3e8c56e400465e
Raja Babu <rajababuk984@gmail.com>
2022-09-30 01:40:44 +0530

    complete code half comment

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -62,7 +62,123 @@ def octant_longest_subsequence_count():
         datain.loc[5,'Octant Id']='-3'
         datain.loc[6,'Octant Id']='+4'
         datain.loc[7,'Octant Id']='-4'
-        
+        #till here matrix has been made now we have to fill the values in the dataframe
+        scp1=scn1=scp2=scn2=scp3=scn3=scp4=scn4=0 #made variable for subsequent positive and negative count
+        cp1=cn1=cp2=cn2=cp3=cn3=cp4=cn4=0 #made variables for count the appearance of subsequent count
+        for i in range(0,total_size):
+            if(datain.loc[i,'Octant']==1): 
+                count=0 #initiated local variable which will help in updating sc#@(p/n)(1/2/3/4)
+                while (datain.loc[i,'Octant']==1): #loop for checking appearance of velocity in octant
+                    count+=1 #updating count
+                    i+=1 #updating rows for checking continuity in a octant
+                    if(i>=total_size):
+                        break #this is because we have only rows upto index total size-1 and .loc func will unable to access the rows which actually does not exist
+                if(count>scp1):
+                    scp1=count #updating subsequent longest  count 
+                    cp1=1 #assigning value as 1 because of new longest subsequent count
+                elif (count==scp1):
+                    cp1+=1
+            elif(datain.loc[i,'Octant']==-1):
+                count=0
+                while (datain.loc[i,'Octant']==-1):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scn1):
+                    scn1=count
+                    cn1=1
+                elif (count==scn1):
+                    cn1+=1
+            elif(datain.loc[i,'Octant']==2):
+                count=0
+                while (datain.loc[i,'Octant']==2):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scp2):
+                    scp2=count
+                    cp2=1
+                elif (count==scp2):
+                    cp2+=1
+            elif(datain.loc[i,'Octant']==-2):
+                count=0
+                while (datain.loc[i,'Octant']==-2):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scn2):
+                    scn2=count
+                    cn2=1
+                elif (count==scn2):
+                    cn2+=1
+            elif(datain.loc[i,'Octant']==3):
+                count=0
+                while (datain.loc[i,'Octant']==3):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scp3):
+                    scp3=count
+                    cp3=1
+                elif (count==scp3):
+                    cp3+=1
+            elif(datain.loc[i,'Octant']==-3):
+                count=0
+                while (datain.loc[i,'Octant']==-3):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scn3):
+                    scn3=count
+                    cn3=1
+                elif (count==scn3):
+                    cn3+=1
+            elif(datain.loc[i,'Octant']==4):
+                count=0
+                while (datain.loc[i,'Octant']==4):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scp4):
+                    scp4=count
+                    cp4=1
+                elif (count==scp4):
+                    cp4+=1
+            elif(datain.loc[i,'Octant']==-4):
+                count=0
+                while (datain.loc[i,'Octant']==-4):
+                    count+=1
+                    i+=1
+                    if(i>=total_size):
+                        break
+                if(count>scn4):
+                    scn4=count
+                    cn4=1
+                elif (count==scn4):
+                    cn4+=1
+            i=i-1
+        datain.loc[0,'Longest Subsequence Length']=scp1
+        datain.loc[0,'Count']=cp1
+        datain.loc[1,'Longest Subsequence Length']=scn1
+        datain.loc[1,'Count']=cn1
+        datain.loc[2,'Longest Subsequence Length']=scp2
+        datain.loc[2,'Count']=cp2
+        datain.loc[3,'Longest Subsequence Length']=scn2
+        datain.loc[3,'Count']=cn2
+        datain.loc[4,'Longest Subsequence Length']=scp3
+        datain.loc[4,'Count']=cp3
+        datain.loc[5,'Longest Subsequence Length']=scn3
+        datain.loc[5,'Count']=cn3
+        datain.loc[6,'Longest Subsequence Length']=scp4
+        datain.loc[6,'Count']=cp4
+        datain.loc[7,'Longest Subsequence Length']=scn4
+        datain.loc[7,'Count']=cn4
         try:
             datain.to_excel('output_octant_longest_subsequence.xlsx',index=False)# it makes a csv file with the name given in 'quote'
             print("Your desired output file is ready!!! Please Check!")
